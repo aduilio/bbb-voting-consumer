46089fb8b99277bf0e7580e732266f5e5c6bf3e9
Aduilio Silva <developer.aduilio@gmail.com>
2022-04-19 12:48:54 -0300

    Start project
    - This project will be responsible to read the messages from Kafka and save them in the database.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Big Brother Brasil - Voting System
+
+Project created during DIO LiveCoding. A Spring Boot project with Angular. The database is MongoDB and Kafka for messaging.
---
+++ src/test/java/com/aduilio/votacaobbb/Bbb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduilio.votacaobbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
